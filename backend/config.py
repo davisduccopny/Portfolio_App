96b8f15f8d03092cf58596b4c098cdf6f15daf81
Hoang Xuan Quoc <2156210125@hcmussh.edu.vn>
2025-04-03 17:06:23 +0700

    commit config

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,6 @@
+import os
 BASE_URL = "http://localhost:8000"
 ORIGINS_URL = ["http://127.0.0.1:5500", "http://localhost:5500"]
-SECRET_KEY = "mysecretkey"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
